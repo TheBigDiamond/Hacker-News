d50f981f8c05987044d702bc729f0f7006770f0f
Luca <lucanuzzo26@gmail.com>
2025-07-08 18:57:28 +0200

    updated code to make it lighter for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -53,6 +54,11 @@ module.exports = {
     },
   },
   plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'public', to: '' },
+      ],
+    }),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       filename: "index.html",
